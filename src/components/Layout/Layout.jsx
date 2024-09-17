141bc80ba170604df9f1fc680fc2d06a7189f3b6
Islomjonsdev <islomjon@immigrateable.com>
2024-09-17 16:41:01 +0500

    fixed bugs

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,7 +2,6 @@ import React, { Fragment } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
-import HeaderBottom from "./Header/HeaderBottom/HeaderBottom";
 
 const Layout = () => {
   return (
