2e58a6b08e80e3f6bdb6e2d324c1f0b58738cb55
Islomjonsdev <islomjon@immigrateable.com>
2024-09-19 16:06:54 +0500

    hero component done

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,13 +3,14 @@ import { useState } from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
 import { FcLike } from "react-icons/fc";
 import { Icons } from "../../SvgIcons";
+import chairImg from "../../assets/images/chair.png"
 import style from "./Hero.module.scss";
 
 const Hero = () => {
-  const [like, setLike] = useState(false)
+  const [like, setLike] = useState(false);
   const handleLike = () => {
-    setLike((prev) => !prev)
-  }
+    setLike((prev) => !prev);
+  };
   return (
     <section className={style.hero}>
       <div className={style.container}>
@@ -19,9 +20,9 @@ const Hero = () => {
               <FaArrowLeft />
             </button>
             <div className={style.hero_left}>
-                <h3>Самый мягкий диваны</h3>
-                <p>Сейфы и металлические шкафы</p>
-                <span>ПРОМЕТ</span>
+              <h3>Самый мягкий диваны</h3>
+              <p>Сейфы и металлические шкафы</p>
+              <span>ПРОМЕТ</span>
             </div>
             <button className={style.hero_btn} data-btn-type="right">
               <FaArrowRight />
@@ -29,12 +30,24 @@ const Hero = () => {
           </div>
 
           <div className={style.hero_right}>
-              <div className={style.hero_right_top}>
-                  <h3>товар дня</h3>
-                  <button onClick={handleLike}>
-                    {like === false ?  <Icons.likeIcon /> : <FcLike />}
-                  </button>
+            <div className={style.hero_right_top}>
+              <h3>товар дня</h3>
+              <button onClick={handleLike}>
+                {like === false ? <Icons.likeIcon /> : <FcLike />}
+              </button>
+            </div>
+            <div className={style.hero_right_bottom}>
+              <img src={chairImg} alt="" />
+
+              <div className={style.hero_right_bottom_list}>
+                  <div>
+                    <p className={style.text1}>Cкидка</p>
+                    <span>-90%</span>
+                  </div>
+                  <p className={style.text2}>Нет отзывов</p>
+                  <span>Realme Pad Mini 32GB + 3GB LTE (синий)</span>
               </div>
+            </div>
           </div>
         </div>
       </div>
