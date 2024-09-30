dcf75a9a8a2aaa4809d346d68026d9b579126348
Islomjonsdev <islomjon@immigrateable.com>
2024-09-30 10:55:12 +0500

    cart length done

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import cartImage from "../../assets/images/cart.png";
+import style from "./Cart.module.scss";
 
 const Cart = () => {
   const accept = useSelector((state) => state);
-  console.log(accept);
   return (
-    <div>
-      {accept?.joinedCart?.cart?.map((item) => (
-       <div>
-          <img src={item?.image} alt="" />
-          <p>{item?.price}</p>
-       </div>
-      ))}
+    <div className={style.cart}>
+      <div className={style.container}>
+        <div className={style.cart_top}>
+          {accept?.joinedCart?.cart?.length > 0 ? (
+            <h1>Savat</h1>
+          ) : (
+            <div>
+              <img src={cartImage} alt="cartImage" />
+              <h1>Корзина пуста</h1>
+              <Link to={"/"}>На главную</Link>
+            </div>
+          )}
+        </div>
+        {accept?.joinedCart?.cart?.map((item) => (
+          <div>
+            <img src={item?.image} alt="" />
+            <p>{item?.price}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
