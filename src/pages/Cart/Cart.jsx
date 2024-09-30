52d4e38aa577843f8acb53ed9d3f75ab83ec6d06
Islomjonsdev <islomjon@immigrateable.com>
2024-09-30 12:40:04 +0500

    cart localStorage done

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,21 +1,32 @@
-import React from "react";
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import cartImage from "../../assets/images/cart.png";
 import style from "./Cart.module.scss";
 
 const Cart = () => {
+  const [cartItems, setCartItems] = useState([]);
   const accept = useSelector((state) => state);
 
   useEffect(() => {
-    
-  }, [])
+    const savedCart = localStorage.getItem("cart");
+    if (savedCart) {
+      setCartItems(JSON.parse(savedCart));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (accept?.joinedCart?.cart && accept.joinedCart.cart.length > 0) {
+      setCartItems(accept.joinedCart.cart);
+      localStorage.setItem("cart", JSON.stringify(accept.joinedCart.cart)); 
+    }
+  }, [accept]);
+
   return (
     <div className={style.cart}>
       <div className={style.container}>
         <div className={style.cart_top}>
-          {accept?.joinedCart?.cart?.length > 0 ? (
+          {cartItems.length > 0 ? (
             <h1>Savat</h1>
           ) : (
             <div>
@@ -25,7 +36,7 @@ const Cart = () => {
             </div>
           )}
         </div>
-        {accept?.joinedCart?.cart?.map((item, id) => (
+        {cartItems.map((item, id) => (
           <div key={id}>
             <img src={item?.image} alt="" />
             <p>{item?.price}</p>
